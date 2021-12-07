25c89a776912d1c4b4c7fa8286b3f5abf542a498
Mattia Sotgia <21292823+mattiasotgia@users.noreply.github.com>
2021-12-07 14:22:58 +0100

    init

---
+++ ErrorAnalysis.h
@@ -0,0 +1,29 @@
+/*
+    ErrorAnalysis - error propagation toolset - C++
+    Author: Mattia Sotgia
+    Version: 0.0 alpha
+*/
+
+#ifndef LABTOOLS_ErrorPropagation
+#define LABTOOLS_ErrorPropagation
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<cmath>
+
+#include<TFormula.h>
+#include<TMath.h>
+
+class ErrorAnalysis{
+
+    private:
+    std::string tFormula;
+
+    public:
+    ErrorAnalysis();
+
+
+}
+
+#endif
